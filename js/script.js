3b53e7212e1367cb8f24217427f240eeea231aed
Sergjio97 <sergiocizmja@gmail.com>
2021-11-29 17:21:57 +0100

    creazione array

--- js/script.js
+++ js/script.js
@@ -2,4 +2,42 @@ const app = new Vue ({
 
     el: "#root",
 
+    data : {
+
+        todos: [
+
+            {
+                text: "Lillà e uva spina",
+                done: false
+            },
+
+            {
+                text: "Trova Yennefer di Vengerberg",
+                done: false
+            },
+
+            {
+                text: "Un amico nei guai",
+                done: false
+            },
+
+            {
+                text: "Trova e libera Dandelion",
+                done: false
+            },
+
+            {
+                text: "Negromante",
+                done: false
+            },
+
+            {
+                text: "Aiuta Yennefer a estrarre informazioni dal cadavere di Skjall",
+                done: false
+            },
+
+        ],
+
+    },
+
 });
